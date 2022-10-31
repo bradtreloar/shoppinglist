a8f5c6f11cebb1d1ed28c4c103dcbc80e54aee90
Brad Treloar <brad.treloar@pm.me>
2022-10-31 19:54:39 +1030

    Remove quantity from Product

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,14 +1,12 @@
 class Product {
   final String description;
-  final int quantity;
   final String? uom;
 
-  Product({required this.description, required this.quantity, this.uom});
+  Product({required this.description, this.uom});
 
   Map<String, dynamic> toMap() {
     return {
       'description': description,
-      'quantity': quantity,
       'uom': uom,
     };
   }
--- test/models/product_test.dart
+++ test/models/product_test.dart
@@ -3,24 +3,20 @@ import '../fakes.dart';
 import 'package:shopping_list/models/product.dart';
 
 void main() {
-  test('Product has required description and quantity', () {
+  test('Product has required description', () {
     final description = randomDescription();
-    final quantity = randomQuantity(1, 10);
 
-    final product = Product(description: description, quantity: quantity);
+    final product = Product(description: description);
 
     expect(product.description, description);
-    expect(product.quantity, quantity);
   });
 
   test('Product has optional unit of measure', () {
     final description = randomDescription();
-    final quantity = randomQuantity(1, 10);
     final uom = randomUom();
 
-    final product1 =
-        Product(description: description, quantity: quantity, uom: uom);
-    final product2 = Product(description: description, quantity: quantity);
+    final product1 = Product(description: description, uom: uom);
+    final product2 = Product(description: description);
 
     expect(product1.uom, uom);
     expect(product2.uom, null);
@@ -28,36 +24,30 @@ void main() {
 
   test('Product converts to Map', () {
     final description = randomDescription();
-    final quantity = randomQuantity(1, 10);
     final uom = randomUom();
 
-    final product1 =
-        Product(description: description, quantity: quantity, uom: uom);
-    final product2 = Product(description: description, quantity: quantity);
+    final product1 = Product(description: description, uom: uom);
+    final product2 = Product(description: description);
 
     final result1 = product1.toMap();
     final result2 = product2.toMap();
 
     expect(result1, {
       'description': description,
-      'quantity': quantity,
       'uom': uom,
     });
     expect(result2, {
       'description': description,
-      'quantity': quantity,
       'uom': null,
     });
   });
 
   test('Product has string representation', () {
     final description = randomDescription();
-    final quantity = randomQuantity(1, 10);
     final uom = randomUom();
 
-    final product1 =
-        Product(description: description, quantity: quantity, uom: uom);
-    final product2 = Product(description: description, quantity: quantity);
+    final product1 = Product(description: description, uom: uom);
+    final product2 = Product(description: description);
 
     expect(product1.toString(), 'Product{$description, UOM: $uom}');
     expect(product2.toString(), 'Product{$description}');
