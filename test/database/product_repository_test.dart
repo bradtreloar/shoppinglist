484ad52af4ca8b746fd78ff4fb35f812c0f688b4
Brad Treloar <brad.treloar@pm.me>
2022-11-03 22:00:32 +1030

    Test edge cases

--- test/database/product_repository_test.dart
+++ test/database/product_repository_test.dart
@@ -1,12 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
 import 'package:shopping_list/models/product.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite/sqflite.dart';
 import '../fakes.dart';
 import 'package:shopping_list/database/product_repository.dart';
 
-@GenerateNiceMocks([])
+Future<Database> inMemoryDatabase() async {
+  return await openDatabase(inMemoryDatabasePath, version: 1,
+      onCreate: (database, version) async {
+    await database.execute('''
+      CREATE TABLE products (
+        id INTEGER PRIMARY KEY,
+        description TEXT,
+        uom TEXT
+      )
+    ''');
+  });
+}
+
 Future main() async {
   setUpAll(() {
     sqfliteFfiInit();
@@ -26,25 +37,26 @@ Future main() async {
   });
 
   test('inserts Product into database', () async {
-    final database = await openDatabase(inMemoryDatabasePath, version: 1,
-        onCreate: (database, version) async {
-      await database.execute('''
-        CREATE TABLE products (
-          id INTEGER PRIMARY KEY,
-          description TEXT,
-          uom TEXT
-        )
-      ''');
-    });
+    final database = await inMemoryDatabase();
     final product1 = Product(description: randomDescription());
     final product2 =
         Product(description: randomDescription(), uom: randomUom());
     final productRepo = ProductRepository(database: database);
 
+    final result0 = await database.query(ProductRepository.tableName);
     await productRepo.insert(product1);
+    final result1 = await database.query(ProductRepository.tableName);
     await productRepo.insert(product2);
+    final result2 = await database.query(ProductRepository.tableName);
 
-    expect(await database.query(ProductRepository.tableName), [
+    expect(result0, []);
+    expect(result1, [
+      {
+        ...product1.toMap(),
+        'id': 1,
+      }
+    ]);
+    expect(result2, [
       {
         ...product1.toMap(),
         'id': 1,
@@ -57,26 +69,19 @@ Future main() async {
   });
 
   test('fetches products from database', () async {
-    final database = await openDatabase(inMemoryDatabasePath, version: 1,
-        onCreate: (database, version) async {
-      await database.execute('''
-        CREATE TABLE products (
-          id INTEGER PRIMARY KEY,
-          description TEXT,
-          uom TEXT
-        )
-      ''');
-    });
+    final database = await inMemoryDatabase();
     final productAttributes = {
       'description': randomDescription(),
       'uom': randomUom(),
     };
-    await database.insert('products', productAttributes);
     final productRepo = ProductRepository(database: database);
 
-    final result = await productRepo.getAll();
+    final result0 = await productRepo.getAll();
+    await database.insert('products', productAttributes);
+    final result1 = await productRepo.getAll();
 
-    expect(result.toList(), [
+    expect(result0.toList(), []);
+    expect(result1.toList(), [
       Product.fromMap({
         'id': 1,
         ...productAttributes,
